95ccc18a81f31752398d547901f0d01fe4c01dff
loren <lorenmichaelh@gmail.com>
2022-01-12 16:35:49 -0600

    completed all deliverables

--- src/index.js
+++ src/index.js
@@ -6,15 +6,13 @@
 
 //✅ Click on an image from the #ramen-menu div and see all the info about that ramen displayed inside the #ramen-detail div and where it says insert comment here and insert rating here.
 
-// Create a new ramen after submitting the new-ramen form.
-    // The new ramen should be added to the#ramen-menu div.
-    // The new ramen does not need to persist; in other words, if you refresh the page, it's okay that the new ramen is no longer on the page.
+//✅ Create a new ramen after submitting the new-ramen form.
+    //✅ The new ramen should be added to the#ramen-menu div.
+    //✅ The new ramen does not need to persist; in other words, if you refresh the page, it's okay that the new ramen is no longer on the page.
 
 
 
 // Assignments
-
-
 
 const baseURL = 'http://localhost:3000';
 const baseURLRamens = 'http://localhost:3000/ramens';
@@ -26,10 +24,7 @@ const detailRestaurant = document.querySelector(".restaurant");
 const detailRating = document.querySelector("#rating-display");
 const detailComment = document.querySelector("#comment-display");
 const newRamenForm = document.querySelector("#new-ramen");
-
-
 // console.log(detailRating)
-
 
 
 // Fetches
@@ -47,18 +42,14 @@ function getDetails(e) {
 }
 
 
-
 // Rendering
 
 function renderAllRamens(ramenArr) {
     ramenArr.forEach(renderOneRamen)
-    // console.log("ramen array", ramenArr)
 }
-
 
 function renderOneRamen(ramenObj) {
     const ramenDiv = document.createElement("img");
-    // console.log(ramenObj)
     ramenDiv.src = ramenObj.image;
     ramenDiv.id = ramenObj.id;
     ramenDiv.addEventListener('click', getDetails)
@@ -66,7 +57,6 @@ function renderOneRamen(ramenObj) {
 }
 
 function renderDetails(ramenObj) {
-    // console.log(ramenObj)
     detailImage.src = ramenObj.image;
     detailName.innerText = ramenObj.name;
     detailRestaurant.innerText = ramenObj.restaurant;
@@ -75,15 +65,21 @@ function renderDetails(ramenObj) {
 }
 
 
-
-// Event Handlers
-
-
-
 // Initializers
 
 const init = () => {
-    getRamens()
+    getRamens();
+
+newRamenForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newRamenObj = {};
+    newRamenObj.name = document.querySelector("#new-name").value;
+    newRamenObj.restaurant = document.querySelector("#new-restaurant").value;
+    newRamenObj.image = document.querySelector("#new-image").value;
+    newRamenObj.rating = document.querySelector("#new-rating").value;
+    newRamenObj.comment = document.querySelector("#new-comment").value;
+    renderOneRamen(newRamenObj)
+})    
 }
 
 document.addEventListener("DOMContentLoaded", init)
