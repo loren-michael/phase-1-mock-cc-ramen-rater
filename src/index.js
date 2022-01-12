4400beac6328411957ca40d3469eb7287bd59e14
loren <lorenmichaelh@gmail.com>
2022-01-12 11:47:35 -0600

    setting up deliverables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-// write your code here
+// Deliverables
+
+// See all ramen images in the div with the id of ramen-menu.
+    // When the page loads, request the data from the server to get all the ramen objects.
+    // Then, display the image for each of the ramen using an img tag inside the #ramen-menu div.
+
+// Click on an image from the #ramen-menu div and see all the info about that ramen displayed inside the #ramen-detail div and where it says insert comment here and insert rating here.
+
+// Create a new ramen after submitting the new-ramen form.
+    // The new ramen should be added to the#ramen-menu div.
+    // The new ramen does not need to persist; in other words, if you refresh the page, it's okay that the new ramen is no longer on the page.
+
+
